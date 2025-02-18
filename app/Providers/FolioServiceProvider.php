84c51de480e236a3d68741bf845d8f2f1aa5d526
amirul <amirulhakim5430@gmail.com>
2025-02-18 12:38:28 +0800

    Add Folio Service provider and routing folder and page of Hello World the first step of laravel

---
+++ app/Providers/FolioServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Folio\Folio;
+
+class FolioServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void    
+    {
+        Folio::domain('order.merchant-portal.test')->path(resource_path('views/pages/order'));
+    }
+}
